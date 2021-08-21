13f191b91eb56b9af019f8380b86031a935b0803
Bruno Bortolato <bbortolato31@gmail.com>
2021-08-21 16:34:25 -0300

    primeiro_commit

---
+++ fundamentos/array.js
@@ -0,0 +1,9 @@
+const valores = [7.7, 8.9, 9.2]
+
+valores[4] = 10
+
+valores.length
+valores.push()
+valores.pop()
+
+delete valores[0]
---
+++ fundamentos/organizacao.js
@@ -0,0 +1,18 @@
+console.log("teste")
+
+// STRINGS
+
+const escola = "Coder"
+console.log(escola.charAt(4))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('e'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0,3))
+
+console.log("Escola ".concat(escola).concat("!"))
+console.log("Escola " + escola + "!")
+console.log(escola.replace("e", 3))
+
+console.log("Ana,Maria,Pedro".split(','))
+
