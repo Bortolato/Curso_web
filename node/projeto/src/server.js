c74201f58b59f74f56efbeb6b69c71064d1e3761
Bruno Bortolato <bbortolato31@gmail.com>
2021-08-24 00:05:22 -0300

    express api

--- node/projeto/src/server.js
+++ node/projeto/src/server.js
@@ -2,6 +2,7 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
     console.log('Middleware 1...')
@@ -9,7 +10,19 @@ app.get('/produtos', (req, res, next) => {
 })
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45 }) //converter para JSON
+    res.send(bancoDeDados.getProdutos()) //converter para JSON
+    next()
+})
+
+app.get('/produtos/:id',(req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next)=> {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
 })
 
 app.listen(porta, () =>{
